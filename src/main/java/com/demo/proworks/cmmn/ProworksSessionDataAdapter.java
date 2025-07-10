e46e71fe4648a8cdd56cd3b84fe5a4afec84652c
kyungwon2won2 <ykw1230@naver.com>
2025-07-10 14:11:32 +0900

    feat : session 생성 process, 방생성 미리보기, conference room

--- src/main/java/com/demo/proworks/cmmn/ProworksSessionDataAdapter.java
+++ src/main/java/com/demo/proworks/cmmn/ProworksSessionDataAdapter.java
@@ -3,6 +3,7 @@ package com.demo.proworks.cmmn;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 import com.inswave.elfw.adapter.AdapterException;
 import com.inswave.elfw.exception.ElException;
@@ -15,21 +16,19 @@ import com.demo.proworks.emp.vo.EmpVo;
 import com.demo.proworks.user.service.UserService;
 import com.demo.proworks.user.vo.UserVo;
 
-/**  
+/**
  * @Class Name : ProworksSessionDataAdapter.java
- * @Description : 프로젝트 세션 데이터 어댑터 - 로그인 후 사용자 헤더 정보를 Setting 한다. 
- * @Modification Information  
- * @
- * @  수정일                  수정자                  수정내용
- * @ ---------   ---------   -------------------------------
- * @ 2019.08.01   EL개발팀             최초생성
+ * @Description : 프로젝트 세션 데이터 어댑터 - 로그인 후 사용자 헤더 정보를 Setting 한다.
+ * @Modification Information
+ * @ @ 수정일 수정자 수정내용 @ --------- --------- ------------------------------- @
+ *   2019.08.01 EL개발팀 최초생성
  * 
  * @author EL개발팀
  * @since 2013.08.01
  * @version 1.0
  * @see
  * 
- *  Copyright (C) by Inswave All right reserved.
+ *      Copyright (C) by Inswave All right reserved.
  */
 public class ProworksSessionDataAdapter extends SessionDataAdapter {
 	/**
@@ -42,21 +41,20 @@ public class ProworksSessionDataAdapter extends SessionDataAdapter {
 	}
 
 	/**
-	 * 데모용 세션 터이터의 로드를 담당하는 구현체 메소드.
-	 * - 프레임워크 SessionDataAdapter 추상클래스의 세션 데이터를 Set 하는 구현체 메소드
-	 * - 프로젝트에 필요한 헤더 정보를 세팅한다. 
-	 * -  해당 헤더 정보는 로그인 후에 사용가능하다. 
+	 * 데모용 세션 터이터의 로드를 담당하는 구현체 메소드. - 프레임워크 SessionDataAdapter 추상클래스의 세션 데이터를 Set
+	 * 하는 구현체 메소드 - 프로젝트에 필요한 헤더 정보를 세팅한다. - 해당 헤더 정보는 로그인 후에 사용가능하다.
 	 * 
 	 * @param request HttpServletRequest
 	 * @param id
-	 * @param obj 기타 동적 파라미터에 추가할 수 있다.(ex. 서비스 구현체 )
+	 * @param obj     기타 동적 파라미터에 추가할 수 있다.(ex. 서비스 구현체 )
 	 * @return ProworksUserHeader
 	 * @throws AdapterException
 	 */
 	@Override
-	public ProworksUserHeader setSessionData(HttpServletRequest request, String id, Object... obj) throws AdapterException{
-		
-		// 로그인 후에 id 기반으로 세션 정보를 세팅하여 반환한다.		
+	public ProworksUserHeader setSessionData(HttpServletRequest request, String id, Object... obj)
+			throws AdapterException {
+
+		// 로그인 후에 id 기반으로 세션 정보를 세팅하여 반환한다.
 		ProworksUserHeader userHeader = new ProworksUserHeader();
 		userHeader.setUserId(id);
 		userHeader.setEmail(id);
@@ -84,36 +82,46 @@ public class ProworksSessionDataAdapter extends SessionDataAdapter {
 //			throw new AdapterException("EL.ERROR.LOGIN.0005");
 //		}
 
-try{
-			UserService userService = (UserService)ElBeanUtils.getBean("userServiceImpl");
+		try {
+			UserService userService = (UserService) ElBeanUtils.getBean("userServiceImpl");
 			UserVo userVo = new UserVo();
-			System.out.println("아이디야~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~세션어뎁터``"+id);
+			System.out.println("아이디야~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~세션어뎁터``" + id);
 			userVo.setEmail(id);
-			userVo.setTenantId((String)obj[1]);
-			System.out.println("이거 서비스로 넘어가는 vo"+userVo);
+			userVo.setTenantId((String) obj[1]);
+			System.out.println("이거 서비스로 넘어가는 vo" + userVo);
 			UserVo resuserVo = userService.loginUser(userVo);
 
-			if( resuserVo == null ) {
-				throw new AdapterException("EL.ERROR.LOGIN.0004", new String[]{id});
+			if (resuserVo == null) {
+				throw new AdapterException("EL.ERROR.LOGIN.0004", new String[] { id });
 			}
 //			userHeader.setTenantId(resuserVo.getTenantId());
 //			userHeader.setIsActive(resuserVo.isIsActive());
 //			userHeader.setRole(resuserVo.getRole());
-			System.out.println("유저헤더 값이야 설정해놓은거"+userHeader);
-			
-			
-			
+
+////-------------------------내가 바꾼거야 - 경원 ----------------------------------------------------------------------------
+//			HttpSession session = request.getSession();
+//			session.setAttribute("tenantId", resuserVo.getTenantId());
+//			session.setAttribute("userRole", resuserVo.getRole());
+//			session.setAttribute("isActive", resuserVo.isIsActive());
+//
+//			System.out.println("세션에 저장된 tenantId: " + resuserVo.getTenantId());
+//			System.out.println("세션에 저장된 userRole: " + resuserVo.getRole());
+//
+//			System.out.println("유저헤더 값이야 설정해놓은거" + userHeader);
+////-------------------------내가 바꾼거야----------------------------------------------------------------------------
+
+
 			// 사용자 세션 설정
-			//userHeader.setTestDeptNo(resEmpVo.getDeptno());
-			//userHeader.setTestDeptName(resEmpVo.getDname());
-		}catch(ElException e){
-			AppLog.error("setSessionData Error1",e);
+			// userHeader.setTestDeptNo(resEmpVo.getDeptno());
+			// userHeader.setTestDeptName(resEmpVo.getDname());
+		} catch (ElException e) {
+			AppLog.error("setSessionData Error1", e);
 			throw e;
-		}catch(Exception e){
-			AppLog.error("setSessionData Error2",e);
+		} catch (Exception e) {
+			AppLog.error("setSessionData Error2", e);
 			throw new AdapterException("EL.ERROR.LOGIN.0005");
 		}
-		
+
 		return userHeader;
 	}
 
